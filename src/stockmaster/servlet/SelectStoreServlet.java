f1b8207e50c3021e19c2e4772c6d8a753b9d9ef8
rui042 <spr2440007@stu.o-hara.ac.jp>
2025-11-06 09:48:50 +0900

    マップ表示エラー修正

--- src/stockmaster/servlet/SelectStoreServlet.java
+++ src/stockmaster/servlet/SelectStoreServlet.java
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,16 +32,26 @@ public class SelectStoreServlet extends HttpServlet {
       Class.forName("org.h2.Driver");
 
       try (Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/stockmaster;MODE=MySQL", "sa", "");
-           PreparedStatement stmt = conn.prepareStatement("SELECT STORE_ID, STORE_NAME, STORE_ADDRESS FROM STORES");
+           PreparedStatement stmt = conn.prepareStatement("SELECT STORE_ID, STORE_NAME, STORE_ADDRESS, STORE_PHONE, OPEN_TIME, CLOSE_TIME FROM STORES");
            ResultSet rs = stmt.executeQuery()) {
+
+    	  DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
         while (rs.next()) {
           int id = rs.getInt("STORE_ID");
           String name = rs.getString("STORE_NAME");
           String address = rs.getString("STORE_ADDRESS");
-          storeList.add(new StoreBean(id, name, address));
-        }
 
+          String phone = rs.getString("STORE_PHONE");
+       // 時刻を LocalTime に変換
+          String openTimeStr = rs.getString("OPEN_TIME");
+          String closeTimeStr = rs.getString("CLOSE_TIME");
+          LocalTime openTime = LocalTime.parse(openTimeStr, timeFormatter);
+          LocalTime closeTime = LocalTime.parse(closeTimeStr, timeFormatter);
+
+          StoreBean store = new StoreBean(id, name, address, phone, openTime, closeTime);
+          storeList.add(store);
+        }
       }
 
       // 正常時にJSPへ遷移
